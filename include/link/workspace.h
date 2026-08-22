26724aae20c2323c8baec59ef35f464ebeeec178
Shannon Smith <infiltratr@yandex.com>
2026-08-22 15:51:57 +1000

    Add JAGLINK forwarding header for LINK workspace

---
+++ include/link/workspace.h
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+#ifndef JAGLINK_LINK_WORKSPACE_FORWARD_H
+#define JAGLINK_LINK_WORKSPACE_FORWARD_H
+
+#include "../../src/link/include/link/workspace.h"
+
+#endif

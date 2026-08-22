3da353ac0340950b3fb4695fd11ecc41fcfa1e28
Shannon Smith <infiltratr@yandex.com>
2026-08-22 13:18:52 +1000

    Consume shared LINK discover core

--- include/jaglink/discover.h
+++ include/jaglink/discover.h
@@ -2,59 +2,33 @@
 #ifndef JAGLINK_DISCOVER_H
 #define JAGLINK_DISCOVER_H
 
-#include <stddef.h>
-#include <stdint.h>
+#include "link/discover.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+typedef link_safety_decision jaglink_safety_decision;
+#define JAGLINK_SAFETY_BLOCK LINK_SAFETY_BLOCK
+#define JAGLINK_SAFETY_ALLOW_READ_ONLY LINK_SAFETY_ALLOW_READ_ONLY
 
-typedef enum jaglink_safety_decision {
-    JAGLINK_SAFETY_BLOCK = 0,
-    JAGLINK_SAFETY_ALLOW_READ_ONLY = 1
-} jaglink_safety_decision;
+typedef link_safety_reason jaglink_safety_reason;
+#define JAGLINK_SAFETY_REASON_ALLOWED_OBD_READ LINK_SAFETY_REASON_ALLOWED_OBD_READ
+#define JAGLINK_SAFETY_REASON_ALLOWED_UDS_READ LINK_SAFETY_REASON_ALLOWED_UDS_READ
+#define JAGLINK_SAFETY_REASON_EMPTY_REQUEST LINK_SAFETY_REASON_EMPTY_REQUEST
+#define JAGLINK_SAFETY_REASON_WRITE_OR_CONTROL LINK_SAFETY_REASON_WRITE_OR_CONTROL
+#define JAGLINK_SAFETY_REASON_ECU_RESET LINK_SAFETY_REASON_ECU_RESET
+#define JAGLINK_SAFETY_REASON_SECURITY_ACCESS LINK_SAFETY_REASON_SECURITY_ACCESS
+#define JAGLINK_SAFETY_REASON_ROUTINE_CONTROL LINK_SAFETY_REASON_ROUTINE_CONTROL
+#define JAGLINK_SAFETY_REASON_DTC_CLEAR LINK_SAFETY_REASON_DTC_CLEAR
+#define JAGLINK_SAFETY_REASON_PROGRAMMING LINK_SAFETY_REASON_PROGRAMMING
+#define JAGLINK_SAFETY_REASON_DENY_BY_DEFAULT LINK_SAFETY_REASON_DENY_BY_DEFAULT
 
-typedef enum jaglink_safety_reason {
-    JAGLINK_SAFETY_REASON_ALLOWED_OBD_READ = 0,
-    JAGLINK_SAFETY_REASON_ALLOWED_UDS_READ,
-    JAGLINK_SAFETY_REASON_EMPTY_REQUEST,
-    JAGLINK_SAFETY_REASON_WRITE_OR_CONTROL,
-    JAGLINK_SAFETY_REASON_ECU_RESET,
-    JAGLINK_SAFETY_REASON_SECURITY_ACCESS,
-    JAGLINK_SAFETY_REASON_ROUTINE_CONTROL,
-    JAGLINK_SAFETY_REASON_DTC_CLEAR,
-    JAGLINK_SAFETY_REASON_PROGRAMMING,
-    JAGLINK_SAFETY_REASON_DENY_BY_DEFAULT
-} jaglink_safety_reason;
+typedef link_safety_result jaglink_safety_result;
+#define jaglink_safety_classify link_safety_classify
+#define jaglink_safety_reason_string link_safety_reason_string
 
-typedef struct jaglink_safety_result {
-    jaglink_safety_decision decision;
-    jaglink_safety_reason reason;
-    uint8_t service;
-} jaglink_safety_result;
-
-jaglink_safety_result jaglink_safety_classify(const uint8_t *payload, size_t length);
-const char *jaglink_safety_reason_string(jaglink_safety_reason reason);
-
-typedef struct jaglink_evidence_writer jaglink_evidence_writer;
-
-jaglink_evidence_writer *jaglink_evidence_open(const char *path);
-int jaglink_evidence_write_frame(jaglink_evidence_writer *writer,
-                                 uint64_t timestamp_ns,
-                                 const char *direction,
-                                 const char *protocol,
-                                 uint32_t can_id,
-                                 const uint8_t *data,
-                                 size_t length,
-                                 const char *annotation);
-int jaglink_evidence_write_annotation(jaglink_evidence_writer *writer,
-                                      uint64_t timestamp_ns,
-                                      const char *text);
-int jaglink_evidence_flush(jaglink_evidence_writer *writer);
-void jaglink_evidence_close(jaglink_evidence_writer *writer);
-
-#ifdef __cplusplus
-}
-#endif
+typedef link_evidence_writer jaglink_evidence_writer;
+#define jaglink_evidence_open link_evidence_open
+#define jaglink_evidence_write_frame link_evidence_write_frame
+#define jaglink_evidence_write_annotation link_evidence_write_annotation
+#define jaglink_evidence_flush link_evidence_flush
+#define jaglink_evidence_close link_evidence_close
 
 #endif

66c1b281bfaef08be01a407e3f3e7b9123334702
Shannon Smith <infiltratr@yandex.com>
2026-08-22 17:05:18 +1000

    Forward shared LINK parameter store contract

---
+++ include/link/parameter_store.h
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+#ifndef JAGLINK_LINK_PARAMETER_STORE_FORWARD_H
+#define JAGLINK_LINK_PARAMETER_STORE_FORWARD_H
+#include "../../src/link/include/link/parameter_store.h"
+#endif

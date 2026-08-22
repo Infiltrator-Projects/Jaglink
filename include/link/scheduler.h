8f0329c3ec232c5fa81bb8bf94a22bd04b6adbc2
Shannon Smith <infiltratr@yandex.com>
2026-08-22 17:05:30 +1000

    Forward shared LINK scheduler contract

---
+++ include/link/scheduler.h
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+#ifndef JAGLINK_LINK_SCHEDULER_FORWARD_H
+#define JAGLINK_LINK_SCHEDULER_FORWARD_H
+#include "../../src/link/include/link/scheduler.h"
+#endif

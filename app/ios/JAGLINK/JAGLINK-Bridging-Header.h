bfdf52902228f00c1de13325d9b97bdd3e5f998f
Shannon Smith <infiltratr@yandex.com>
2026-09-04 18:08:11 +1000

    JAGLINK: bridge shared LINK vehicle profile store

--- app/ios/JAGLINK/JAGLINK-Bridging-Header.h
+++ app/ios/JAGLINK/JAGLINK-Bridging-Header.h
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 #import "JagLinkDiagnosticsController.h"
+#import "../../src/link/platform/apple/LinkDiagnosticsController.h"
 #include "jaglink/jaguar.h"
 #include "jaglink/jaglink.h"
 #include "jaglink/parameter.h"
